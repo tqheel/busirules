cb3904c81de32214c969a499a4f3e095f0993e0c
Todd Qualls <tqualls@gmail.com>
2015-01-06 22:49:37 -0500

    Adds array methods for iterating through reserved patterns and words in business rules.

--- validate.js
+++ validate.js
@@ -1,19 +1,62 @@
-function validateStem(){
-	var stem = $('#stem').val();
-	console.log(stem);
-	console.log(reservedPatterns[0].reservedPatternText);
-	
+var isValid = true;
+var messages = [];
 
+function checkBusinessRules(){
+  var stem = $('#stem').val();
+  var options = $('.option').map(function(){
+    console.log(this.value);
+    return this.value;
+  }).get();
+  validate(stem, true);
+  validate(options, false);
+  //determine if validation passed (isValid?)
+
+  //if !isValid, add messages to UI
+
+  //reset global variables
 }
 
-function validateOptions(){
-
-	var options = $('.option').map(function(){
-		console.log(this.value);
-		return this.value;
-	}).get();
-
-	console.log(options);
+function validate(input, isStem){
+  var i = 0;
+  var j = 0;
+  if(isStem){
+    var stemPatterns = reservedPatterns.filter(function(pattern){
+      return pattern.applyToStems;
+    });
+    var stemWords = reservedWords.filter(function(word){
+      return word.applyToStems;
+    });
+    for(i=0; i < stemPatterns.length; i++){
+      //evaluate each pattern against the stem
+      console.log(input + ' against ' + stemPatterns[i].reservedPatternText);
+      //if problem with pattern, add the issue to the message array
+    }
+    for(i=0; i < stemWords.length; i++){
+      //evaluate each word against the stem
+      console.log(input + ' against ' + stemWords[i].reservedWordText);
+      //if problem with word, add the issue to the message array
+    }
+  }
+  else{
+    var optionPatterns = reservedPatterns.filter(function(pattern){
+      return pattern.applyToOptions;
+    });
+    var optionWords = reservedWords.filter(function(word){
+      return word.applyToOptions;
+    });
+    for(i=0; i < input.length; i++){
+      for(j=0; j < optionPatterns.length; j++){
+        //evaluate each pattern against the option
+        console.log(input[i] + ' against ' + optionPatterns[j].reservedPatternText);
+        //if problem with pattern, add the issue to the message array
+      }
+      for(j=0; j < optionWords.length; j++){
+        //evaluate each word against the option
+        console.log(input[i] + ' against ' + optionWords[j].reservedWordText);
+        //if problem with word, add the issue to the message array
+      }
+    } 
+  }
 }
 
 var reservedPatterns = [
